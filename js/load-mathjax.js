c558dbcf802d6d4a4fb75f01310c7024f2ad50b2
laomai <skybbx@msn.com>
2022-05-18 18:42:26 +0800

    rebuilding site 2022年05月18日 18:42:18

--- js/load-mathjax.js
+++ js/load-mathjax.js
@@ -6,7 +6,7 @@ window.MathJax = {
   
   (function () {
     var script = document.createElement('script');
-    script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js';
+    script.src = 'https://cdn.qylao.com/dist/mathjax/es5/tex-chtml.js';
     script.async = true;
     document.head.appendChild(script);
   })();
